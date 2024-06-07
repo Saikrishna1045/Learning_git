9ddf2a2b0125b5e99c8d49fa308f2a3baa644df2
[sai cris] <[eccentric1045@gmail.com]>
2024-06-07 12:16:40 +0530

    modify

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("this is my second version")
+import time
+print("this is my Third version")
+start = time.time()
+word_list = ['Cristiano', ',', ' Ratan Tata',' and',' SK', ' are', ' my', ' inspiration.']
+# for word in word_list:
+#     new_list += word
+new_list1 = "".join(word_list)
+print(new_list1)
+print(time.time() -start, 'seconds')
